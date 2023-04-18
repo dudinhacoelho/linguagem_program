56d11d5b2604569dc1df35ed3cadbcb32db316ef
dudinhacoelho <130070421+dudinhacoelho@users.noreply.github.com>
2023-04-18 08:23:39 -0300

    Add files via upload
    exercício do dia 18/04

---
+++ ativ_aluno.py
@@ -0,0 +1,8 @@
+class aluno:
+
+    def __init__ (self, nome, matricula, prova1, prova2, trabalho1):
+        self.nome = nome 
+        self.matricula = matricula
+        self.prova1 = prova1
+        self.prova2 = prova2
+        self.trabalho1 = trabalho1
---
+++ teste_aluno.py
@@ -0,0 +1,10 @@
+from ativ_aluno import aluno
+
+def infos():
+    nome = input('Digite seu nome: ')
+    matricula = int(input('Digite sua matricula: '))
+    prova1 = int(input('Digite sua primeira nota: '))
+    prova2 = int(input('Digite sua segunda nota: '))
+    trabalho1 = int(input('Digite a nota do trabalho: '))
+
+infos()
